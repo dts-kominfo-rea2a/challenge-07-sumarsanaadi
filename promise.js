58400de57707db3462aa7eb84efa1a61d796b299
sumarsanaadi <sumarsana.adi@gmail.com>
2022-06-17 16:33:57 +0800

    challenge 07 finish

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let dataTheaterIXX = await promiseTheaterIXX();
+  let dataTheaterVGC = await promiseTheaterVGC();
+  let dataCombine = dataTheaterIXX.concat(dataTheaterVGC);
+  let totalEmotion = 0;
+  dataCombine.map(data => data.hasil === emosi && (totalEmotion += 1));
+  return totalEmotion;
+};
 
 module.exports = {
   promiseOutput,
